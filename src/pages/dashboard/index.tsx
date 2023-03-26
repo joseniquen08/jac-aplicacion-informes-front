660587d7a2f4118ddaafc5be3077c7874adfdbf1
KimberlyPocco <kimberly.poccop@gmail.com>
2023-03-25 21:22:02 -0500

    component sidebar and view dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -15,6 +15,7 @@ const Dashboard = ({ users }: any) => {
             </div>
             <div className="w-full lg:w-10/12 py-16 px-12">
               <div className="flex items-center justify-center">
+                {/* Aqui empieza el buscador */}
                 <div className="flex flex-grow">
                   <form className="flex w-full">
                     <input className="flex-1 bg-white border border-gray-300 rounded-l-md py-2 px-4 focus:outline-none" type="text" placeholder="Buscar..." />
@@ -22,9 +23,9 @@ const Dashboard = ({ users }: any) => {
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width={1.5} stroke="currentColor" className="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                       </svg>
-
                     </button>
                   </form>
+                  {/* Aqui se contiene el boton crear proyecto */}
                 </div>
                 <div className="flex ml-4">
                   <button className="bg-blue-500 hover:bg-blue-700 text-white rounded-md py-2 px-4" type="submit">Crear Proyecto</button>
